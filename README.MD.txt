6580fa014ed01a50cd4d761728e42057e02f28c3
noahgribbin <noahgribbin@yahoo.com>
2015-10-15 09:30:10 -0700

    finished pair programming

---
+++ README.MD.txt
@@ -0,0 +1,2 @@
+Driver: Tiffine
+Navigator: Noah
--- app.js
+++ app.js
@@ -1,24 +1,26 @@
 var twowrestlers = [];
 var index;
 var voted = false;
-
-var Wrestler = function(src, pop){
+var data = [];
+var toBeIncreaced;
+var Wrestler = function(src, wrestler){
   this.src = src;
-  this.pop = pop
+  this.name = wrestler;
+  data.push({value:1, label: wrestler,color: '#361A18', highlight: '#811B33'})
 }
 var wrestlers = [
-  new Wrestler('img/batista.png',0),
-  new Wrestler('img/cena.png',0),
-  new Wrestler('img/iron-sheik.png',0),
-  new Wrestler('img/lesnar.png',0),
-  new Wrestler('img/macho-man.png',0),
-  new Wrestler('img/mc-mahon.png',0),
-  new Wrestler('img/mc-mahon2.png',0),
-  new Wrestler('img/orton.png',0),
-  new Wrestler('img/stone-cold.png',0),
-  new Wrestler('img/tripple-h.png',0),
-  new Wrestler('img/undertaker.png',0),
-  new Wrestler('img/ventura.png',0)
+  new Wrestler('img/batista.png','Batista'),
+  new Wrestler('img/cena.png','John Cena'),
+  new Wrestler('img/iron-sheik.png','Iron Sheik'),
+  new Wrestler('img/lesnar.png','Brock Lesnar'),
+  new Wrestler('img/macho-man.png','Macho Man'),
+  new Wrestler('img/mc-mahon.png','Vince McMahon'),
+  new Wrestler('img/mc-mahon2.png','Ultimate Warrior'),
+  new Wrestler('img/orton.png','Randy Orton'),
+  new Wrestler('img/stone-cold.png','Stone Cold Steve Austin'),
+  new Wrestler('img/tripple-h.png','Triple H'),
+  new Wrestler('img/undertaker.png','The Undertaker'),
+  new Wrestler('img/ventura.png','Jesse The Body Ventura')
 ]
 function randomWrestler(){
   return Math.round(Math.random() * (wrestlers.length-1))
@@ -34,12 +36,12 @@ function getTwoWrestlers(){
 function putWrestler1(){
   var div = document.getElementById('img1');
   div.src = wrestlers[twowrestlers[0]].src;
-  document.getElementById("pop1").innerHTML = wrestlers[twowrestlers[0]].pop
+  // document.getElementById("pop1").innerHTML = wrestlers[twowrestlers[0]].pop
 }
 function putWrestler2(){
   var div = document.getElementById('img2');
   div.src = wrestlers[twowrestlers[1]].src;
-  document.getElementById("pop2").innerHTML = wrestlers[twowrestlers[1]].pop
+  // document.getElementById("pop2").innerHTML = wrestlers[twowrestlers[1]].pop
 }
 function hilight1() {
   document.getElementById("img2").style.borderColor = "black";
@@ -54,18 +56,51 @@ function hilight2() {
   voted = true;
 }
 
+function findLabel(array,label){
+  for(var i in array){
+    if (array[i].label === label){
+      return array[i];
+    }
+  }
+}
+
+var votedFor;
+
 function vote(){
   if(voted){
+    console.log(wrestlers[index])
     wrestlers[index].pop += 1;
   }
+  var toBeIncreaced=findLabel(data,wrestlers[index].name);
+  toBeIncreaced.value +=1;
   voted = false;
 }
-function reset(){
+
+function reset() {
   document.getElementById("img1").style.borderColor = "black";
   document.getElementById("img2").style.borderColor = "black";
   twowrestlers = getTwoWrestlers();
   putWrestler1();
   putWrestler2();
   vote();
+  initializeChart();
+};
+
+function initializeChart() {
+  new Chart(ctx).Doughnut(data, {
+    //Number - Amount of animation steps
+    animationSteps : 100,
+    //String - Animation easing effect
+    animationEasing : "easeOutBounce",
+    //Boolean - Whether we animate the rotation of the Doughnut
+    animateRotate : true,
+    //Boolean - Whether we animate scaling the Doughnut from the centre
+    animateScale : true
+  });
 }
+  // line 75 is not being liked by the console
+
+
+
+var ctx = document.getElementById('graph').getContext('2d');
 reset();
